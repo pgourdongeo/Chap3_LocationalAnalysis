9827431158191c65734eb64f92c2506ef7262f16
Unknown <aurelie.douet@gmail.com>
2019-12-03 21:57:37 +0100

    urbact for typo

--- KEEP/AD/SCRIPT/typo_urbact.R
+++ KEEP/AD/SCRIPT/typo_urbact.R
@@ -2,8 +2,7 @@
 #                           TYPOLOGIE DES PROJETS URBACT
 #                         
 #
-# DESCRIPTION : réalisation d'une typologie des projets URBACT en fonction 
-# des villes participantes
+# DESCRIPTION : réalisation d'une typologie des projets URBACT 
 #
 # PG, AD, Novembre 2019
 ##############################################################################
@@ -13,6 +12,7 @@
 #setwd("~/BD_Keep_Interreg/KEEP")
 
 setwd("~/git/Chap3_LocationalAnalysis/KEEP")
+options(scipen = 999)
 
 # Library
 library(readr)
@@ -30,97 +30,70 @@ sfEU <- st_read("AD/FDCARTE/fondEuropeLarge.geojson", crs = 3035)
 
 rec <- st_read("AD/FDCARTE/rec_3035.geojson")
 
-# nutsUR <- st_read("../OtherGeometry/NUTS_UrbainRural.geojson", crs = 3035)
-# NUTS_CP_1420 <- st_read("AD/SHP/NUTS2_StructuralFonds/NUTS2_CohesionPolicy_2014_2020.shp") %>% 
-#   st_transform(3035)
-# NUTS_EF_0613 <-st_read("AD/SHP/NUTS2_StructuralFonds/NUTS2_EuropeanFunds_2006_2013.shp") %>%
-#   st_transform(3035)
-
 urbactCities <- read_delim("AD/URBACT/BdCitiesUrbact_Code_UMZ_LAU2.csv", 
                            ";", escape_double = FALSE, 
                            col_types = cols(Code_Network = col_character()), 
                            trim_ws = TRUE)
 
 
-## Prepare data
-urbactCitiesAggr <- urbactCities %>%
-  mutate(Lead = ifelse(City.Statut == "Lead Partner", 1, 0)) %>%
-  select(CodeCity, Name, X, Y, Country, Region.x, Continent.x, 
-         POPLAU2_2015, ID_UMZ, Pop2011, Lead) %>%
-  group_by(CodeCity) %>%
-  mutate(NbPart = n(), NbLeader = sum(Lead)) %>% 
-  select(-Lead) %>%
-  distinct() %>%
-  filter(!duplicated(CodeCity))
 
-sum(urbactCitiesAggr$NbPart)
-sum(urbactCitiesAggr$NbLeader)
-
-# sfUrbactCitiesAggr <- st_as_sf(urbactCitiesAggr, coords = c("X", "Y"), crs = 4326) %>%
-#   st_sf(sf_column_name = "geometry") %>%
-#   st_transform(crs = 3035)
-# 
-# mapview(sfEU) + mapview(sfUrbactCitiesAggr)
-
-
-# Creation of a new table of URBACT projects
-
+# Creation of a new table of URBACT projects (with number of city in each network)
 network <- urbactCities %>% 
   group_by(Code_Network) %>% 
-  summarise(NcitiesByNw = n()) 
+  summarise(Ncities = n()) 
 
-## % of cities by city size (category) in each project
-# avec le pourcentage de villes par catégorie de taille, 
 
-### create a new var = class of city size (1, 2, 3 ou 4) 
-### si classe de taille validée, nommer ces classes (petite ville, moyenne, etc. par ex)
+# % of cities by city size (category) in each project
+
+## First, create a new vars: class of city size (1, 2, 3 ou 4) 
+## si classe de taille validée, nommer ces classes (petite ville, moyenne, etc. par ex)
 skim(urbactCities$POPLAU2_2015)
 urbactCities <- urbactCities %>% 
-  mutate(KTY_LAU = case_when(POPLAU2_2015 < 50000 ~ "1",
+  mutate(KPOP = case_when(POPLAU2_2015 < 50000 ~ "1",
                          POPLAU2_2015 > 50000 & POPLAU2_2015 < 200000 ~ "2",
                          POPLAU2_2015 > 200000 & POPLAU2_2015 < 500000 ~ "3",
                          TRUE ~ "4"),
          POP_LEAD = ifelse(City.Statut == "Lead Partner", POPLAU2_2015, NA),
-         CITY_LEAD = ifelse(City.Statut == "Lead Partner", Name, NA))
+         CITY_LEAD = ifelse(City.Statut == "Lead Partner", Name, NA)) %>% 
+  group_by(Code_Network) %>% 
+  mutate(Ncountry = length(unique(Country)))
 
-table(urbactCities$KTY_LAU)
+table(urbactCities$KPOP)
 
-### Count number of cities in each category by network
+## Count number of cities in each category by network
 kTY_city <- urbactCities %>% 
-  group_by(Code_Network, KTY_LAU) %>% 
+  group_by(Code_Network, KPOP) %>% 
   summarise(N_Kcity_byNw = n())
 
-
-
-### Add number of cities in each network
+## Add number of cities in each network
 kTY_city <- left_join(kTY_city, network, by = "Code_Network")
 
-### % 
+## % 
 kTY_city <- kTY_city %>% 
-  mutate(P_Kcity_byNw = round(N_Kcity_byNw / NcitiesByNw * 100))
+  mutate(P_Kcity_byNw = round(N_Kcity_byNw / Ncities * 100))
 
 kTY_city_spread <- kTY_city %>% 
-  spread(key = KTY_LAU, value = P_Kcity_byNw, sep = "")
-KTY1 <- kTY_city_spread %>% drop_na(KTY_LAU1) 
-KTY2 <- kTY_city_spread %>% drop_na(KTY_LAU2)
-KTY3 <- kTY_city_spread %>% drop_na(KTY_LAU3) 
-KTY4 <- kTY_city_spread %>% drop_na(KTY_LAU4)  ### too long
+  spread(key = KPOP, value = P_Kcity_byNw, sep = "")
+K1 <- kTY_city_spread %>% drop_na(KPOP1) 
+K2 <- kTY_city_spread %>% drop_na(KPOP2)
+K3 <- kTY_city_spread %>% drop_na(KPOP3) 
+K4 <- kTY_city_spread %>% drop_na(KPOP4)  ### too long
 
 
 network <- network %>% 
-  left_join(select(KTY1, KTY_LAU1)) %>%
-  left_join(select(KTY2, KTY_LAU2)) %>%
-  left_join(select(KTY3, KTY_LAU3)) %>%
-  left_join(select(KTY4, KTY_LAU4))            ### too long
+  left_join(select(K1, KPOP1)) %>%
+  left_join(select(K2, KPOP2)) %>%
+  left_join(select(K3, KPOP3)) %>%
+  left_join(select(K4, KPOP4))            ### too long
 
-rm(KTY1, KTY2, KTY3, KTY4, kTY_city_spread)
+rm(K1, K2, K3, K4, kTY_city_spread, kTY_city)
 
 network <- network %>% 
-  mutate_at(vars("KTY_LAU1", "KTY_LAU2", "KTY_LAU3", "KTY_LAU4"), 
+  mutate_at(vars("KPOP1", "KPOP2", "KPOP3", "KPOP4"), 
             replace_na, 0)
 
 
-# Population of lead partner of each network
+## Population of lead partner for each network
 popLead <- urbactCities %>% 
   filter(!is.na(CITY_LEAD)) %>% 
   select(Code_Network, CITY_LEAD, POP_LEAD)
@@ -129,108 +102,108 @@ network <- network %>%
   left_join(., popLead)
 rm(popLead)
 
-#l'area de la bonding box formée par le semis de point, 
+## le nb de pays concernés 
+Ncountry <- urbactCities %>% 
+  select(Code_Network, Ncountry) %>% 
+  filter(!duplicated(Code_Network))
+
+network <- network %>% left_join(Ncountry)
+
+rm(Ncountry)
+
+## l'area de la bonding box formée par le semis de point 
 sfUrbactCities <- st_as_sf(urbactCities, coords = c("X", "Y"), crs = 4326) %>%
   st_sf(sf_column_name = "geometry") %>%
   st_transform(crs = 3035)
 
 vec <- unique(sfUrbactCities$Code_Network)
-dfarea <- data.frame(Code_Network = vec, AREA_DOTPLOT = double(87))
+dfarea <- data.frame(Code_Network = character(87), area = double(87), row.names = vec)
 for (i in vec) {
   bx <- sfUrbactCities %>% filter(Code_Network == i) %>% st_bbox()
   polyg <- st_as_sfc(st_bbox(bx))
   area <- st_area(polyg)
-  dfarea[, 2] <- area
+  dfarea[, 1] <- vec
+  dfarea[i, 2] <- area
 }
 
-
-## re-format output
-library(purrr)
-library(magrittr)
-
-results <- polyg %>% 
-  map(., "1") %>% 
-str(results[1:3])
-
-ad <- results %>%
-  map(., 1) %>% 
-  map_dfr(., magrittr::extract, "formatted_address")
-
-
-st_as_sf(sfParticipations_snap)
-
-st_sf(sf_column_name = "geometry")
-truc <- st_as_sf(polyg[1])
-
-coord <- as.data.frame(st_coordinates(partners_inEU))
-
-
-
-
+dfarea <- dfarea %>% 
+  mutate(areaKM2 = area/1e+6)
 rm(polyg, bx, area, i, vec)
 network <- network %>% 
-  left_join(., dfarea)
+  left_join(., select(dfarea, Code_Network, areaKM2), by = "Code_Network")
 
 rm(dfarea)
 
-#le nb de pays concernés, 
-#et la distance moyenne ou médiane. 
+
+
+## centre de gravité : point moyen 
+### Point moyen pondéré par la pop
+coords3035 <- as.data.frame(st_coordinates(sfUrbactCities))
+coords3035 <-  coords3035 %>% 
+  mutate(Code_Network = sfUrbactCities$Code_Network,
+         POPLAU2_2015 = sfUrbactCities$POPLAU2_2015)
+PG <- coords3035 %>% 
+  group_by(Code_Network) %>% 
+  summarise(X = mean(X), Y = mean(Y),
+         Xp = weighted.mean(x = X, y = POPLAU2_2015),
+         Yp = weighted.mean(x = Y, y = POPLAU2_2015)) %>% 
+  mutate(CG = "point moyen")
+
+network <- network %>% left_join(select(PG, Code_Network, X_mean = X, Y_mean = Y))
+
+### Point médian marche pas
+require(ICSNP)
+
+vec <- unique(coords3035$Code_Network)
+df <- data.frame(Code_Network = vec, 
+                 medX = numeric(87), medY = numeric(87), row.names = vec)
+for(i in unique(coords3035$Code_Network)){
+  coord <- coords3035 %>% filter(Code_Network == i) %>% select(X, Y)
+  df[i, 2] <-spatial.median(coord)[1]
+  df[i, 3] <-spatial.median(coord)[2]
+}
+
+
+
+
+# Clean envirmnt
+rm(bibi, coord, df, i)
+
+# et la distance moyenne ou médiane. 
+##fonction dist moy au plus proche voisin (Ra)
+MeanDistNN <- function(sf, k){
+  
+  sp <- as(sf, "Spatial")
+  
+  listNearNei <- knearneigh(sp@coords, k , longlat = F)
+  
+  NearNeigh <- knn2nb(listNearNei)
+  
+  distnei <- nbdists(NearNeigh, sp@coords, longlat = F)
+  
+  distnei <- unlist(distnei)
+  
+  meanDist <- mean(distnei)
+  
+  return(meanDist)
+  
+}
+
+## compute for each network
+NNdist <- sfUrbactCities %>%
+  group_by(Code_Network) %>% 
+  do(as.data.frame(MeanDistNN(sf=., k = 1))) %>% 
+  rename(Ra = `MeanDistNN(sf = ., k = 1)` )
+
+network <- network %>% left_join(NNdist)
+
+
+
+
+
 #Après on pourrait faire un ACP, ou même une CAH direct 
 #sur toutes ces grandeurs normalisées pour classifier les projets
 
 
-
-
-PartByCountry <- N %>% left_join(N_UMZCountry)
-
-Participations_ByCountry <- Participations_ByCountry %>% filter(!Country =="NO")
-
-Participations_ByCountry <- Participations_ByCountry %>% mutate(PctParticipationUMZ = (Countryparticipations/NUMZCountry)*100,
-                                                                PctUrbactCitiesUMZ = (Ncities_Country/ NUMZCountry)*100)
-
-Participations_ByCountry <- Participations_ByCountry%>% filter(!PctUrbactCitiesUMZ>100)
-
-ggplot(Participations_ByCountry, aes(x = reorder(Country,-PctUrbactCitiesUMZ), y = PctUrbactCitiesUMZ )) + 
-  geom_bar(stat = "Identity")+
-  labs(title = "Share of cities involved in URBACT by urban morphological zones",
-       x="Country", y = "Percentage Cities involved by UMZ")
-
-#### CAH
-sumStat_resLAU <- urbactCities %>% 
-  group_by(Code_Network) %>% 
-  summarise(Mean_pop15 = mean(POPLAU2_2015), 
-            Med_pop15 = median(POPLAU2_2015),
-            Var_pop15 = var(POPLAU2_2015),
-            Sd_pop15 = sd(POPLAU2_2015),
-            Cv_pop15 = Sd_pop15/Mean_pop15,
-            Min_pop15 = min(POPLAU2_2015),
-            Max_pop15 = max(POPLAU2_2015)) %>% 
-  as.data.frame() %>% 
-  na.omit()
-
-row.names(sumStat_resLAU) <- sumStat_resLAU[,1]
-sumStat_resLAU <- sumStat_resLAU[,-1]
-
-library(GGally)
-ggpairs(sumStat_resLAU)
-sumStat_resLAU <- sumStat_resLAU %>% select(-Sd_pop15,-Max_pop15)
-library(cluster)
-CAH <- agnes(x = sumStat_resLAU, metric="euclidean", method = "average")
-plot(as.dendrogram(CAH), leaflab = "none")
-
-sumStat_resLAU$CAH <- cutree(tree= CAH, k = 6)
-
-sumMeanCAH <- sumStat_resLAU %>% 
-  ungroup() %>% 
-  group_by(CAH) %>%
-  summarise(N = n(),
-            Mean = mean(Mean_pop15),
-            Med = mean(Med_pop15),
-            Var = mean(Var_pop15),
-            #Sd = mean(SdUMZ),
-            Cv = mean(Cv_pop15),
-            Min = mean(Min_pop15)
-            # Max = mean(MaxUMZ))
-  )
 
 
