447cd16360913a09cc228073e1437548ae0c2ad7
DOUET <aurelie.douet@gmail.com>
2021-09-16 16:41:49 +0200

    add propCitiesUrbactP.pdf

--- KEEP/AD/SCRIPT/04_exploration_urbact.R
+++ KEEP/AD/SCRIPT/04_exploration_urbact.R
@@ -59,6 +59,7 @@ urbactCities <- read.csv2("AD/URBACT/UrbactDB_Vfinal/URBACT_Membership_GNidCorr_
 projet <- read.csv2("AD/URBACT/UrbactDB_Vfinal/UrbactNetworks_complete_V2.csv")
 
 
+summary(projet$NbCities)
 
 
 # ================ Prepare data ================
@@ -116,6 +117,8 @@ class(sfUrbactCitiesAggr)
 ## ----~barplot Nb participation/city ----
 
 freq <- as.data.frame(table(urbactCitiesAggr$NbPart))
+
+summary(urbactCitiesAggr$NbPart)
 
 freq_nbpart <- ggplot(data = freq,
        aes(x = Var1, y = Freq)) +
@@ -720,4 +723,179 @@ citiesUrbact <- ggplot() +
 pdf(file = "AD/OUT/propCitiesUrbact2_new.pdf", width = 8.3, height = 5.8, pagecentre = FALSE)
 citiesUrbact
 dev.off()
+
+
+
+# ===== 7. Fig. 3.?: participation par période ======
+
+## Prepare df
+urbactCitiesAggrP <- urbactCities %>%
+  mutate(Lead = ifelse(City.Statut == "Lead Partner", 1, 0)) %>%
+  select(geonameId, asciiName, Lead, Country, Region, Continent, 
+         lng_GN, lat_GN, nbCity, Phase) %>%
+  group_by(geonameId, Phase) %>%
+  mutate(NbPart = n(), NbLeader = sum(Lead)) %>% 
+  select(-Lead) %>%
+  distinct() %>%
+  filter(!duplicated(geonameId)) %>% 
+  filter(!is.na(lat_GN)) %>% 
+  ungroup()
+
+sum(urbactCitiesAggr$NbPart)
+sum(urbactCitiesAggr$NbLeader)
+
+sfUrbactCitiesAggrP <- st_as_sf(urbactCitiesAggrP, coords = c("lng_GN", "lat_GN"), crs = 4326) %>%
+  st_sf(sf_column_name = "geometry") %>%
+  st_transform(crs = 3035) 
+
+summary(sfUrbactCitiesAggrP$NbPart)
+
+
+## prepare nb participations and city by phase
+np <- sfUrbactCitiesAggrP %>% 
+  st_drop_geometry() %>% 
+  group_by(Phase) %>% 
+  summarise(np = sum(NbPart))
+
+nv <- sfUrbactCitiesAggrP %>% 
+  st_drop_geometry() %>% 
+  group_by(Phase) %>% 
+  summarise(nv = length(unique(geonameId)))
+
+n <- np %>% left_join(., nv)
+
+rm(np, nv)
+
+
+## mapping
+citiesUrbactP <- ggplot() + 
+  geom_sf(data = sfEU, fill = "#bfbfbf", color = "white", size = 0.5) +
+  geom_sf(data = sfUrbactCitiesAggrP %>% st_centroid(),
+          mapping = aes(size = NbPart), colour = "#D2019550", show.legend = NA) +
+  geom_sf(data = sfUrbactCitiesAggrP %>% st_centroid(),
+          mapping = aes(size = NbPart), shape = 1, colour = "#D20195", show.legend = NA) +
+  scale_size(name = "Nombre de projets URBACT par ville",
+             breaks = c(9, 4, 2, 1),
+             labels = c("9", "4", "2", "1"),
+             range = c(0.5, 6)) +
+  labs(x = NULL, y = NULL, 
+       caption = "Source : EUCICOP-URBACT, 2019 / PG, AD, 2019") +
+  facet_wrap(~Phase, nrow = 2) +
+  
+  geom_text(data = n, 
+            mapping = aes(x = c(st_bbox(rec)[3]-1000000), 
+                          y = c(st_bbox(rec)[4]-800000), 
+                          label = paste0(np, " participations\n",
+                                         nv, " villes")),
+            size = 2) +
+
+  geom_line(data = myScaleBar, aes(x = X, y = Y), size = 0.5, color = "#333333") +
+  
+  annotate("text", label = "500 km", size = 1.5, color = "#333333", hjust = 0,
+           x = c(st_bbox(rec)[3]-800000), y = c(st_bbox(rec)[2]+280000)) +
+  
+  geom_sf(data = rec, fill = NA, color = "ivory4", size = 0.5) +
+  
+  coord_sf(crs = 3035, datum = NA,
+           xlim = st_bbox(rec)[c(1,3)],
+           ylim = st_bbox(rec)[c(2,4)]) +
+  scale_x_continuous(expand=c(0,0)) +
+  scale_y_continuous(expand=c(0,0)) +
+  theme_void() +
+  theme(legend.position =  c(0.65, 0.36),
+        legend.title = element_text(size = 8),
+        legend.text = element_text(size = 6),
+        strip.text.x = element_text(size = 12),
+        plot.caption = element_text(size = 5.5, vjust = 10, hjust = 0.97))
+
+
+
+## display end save
+pdf(file = "AD/OUT/propCitiesUrbactP.pdf", width = 8.3, height = 5.8, pagecentre = FALSE)
+citiesUrbactP
+dev.off()
+
+
+### map lead partners
+sfUrbactCitiesAggrP <- sfUrbactCitiesAggrP %>% 
+  mutate(NbLeader2 = na_if(NbLeader, 0))
+sfUrbactCitiesAggr <- sfUrbactCitiesAggr %>% 
+  mutate(NbLeader2 = na_if(NbLeader, 0))
+
+## mapping
+citiesUrbactPT <- ggplot() + 
+  geom_sf(data = sfEU, fill = "#bfbfbf", color = "white", size = 0.5) +
+  geom_sf(data = sfUrbactCitiesAggr %>% st_centroid(),
+          mapping = aes(size = NbLeader2), colour = "#D2019550", show.legend = NA) +
+  geom_sf(data = sfUrbactCitiesAggr %>% st_centroid(),
+          mapping = aes(size = NbLeader2), shape = 1, colour = "#D20195", show.legend = NA) +
+  # scale_size(name = "Nombre de projets URBACT par ville",
+  #            breaks = c(9, 4, 2, 1),
+  #            labels = c("9", "4", "2", "1"),
+  #            range = c(0.5, 6)) +
+  # annotate("text", label = "Source : EUCICOP-URBACT, 2019 / PG, AD, 2019",
+  #          size = 2.2, 
+  #          hjust = 1,
+  #          x = c(st_bbox(rec)[3]), y = c(st_bbox(rec)[2]-130000)) +
+  labs(x = "", y = "", 
+       caption = "Source : EUCICOP-URBACT, 2019 / PG, AD, 2019") +
+  # facet_wrap(~Phase, nrow = 2) +
+  
+  # geom_text(data = n, mapping = aes(x = c(st_bbox(rec)[3]-1000000), 
+  #                                   y = c(st_bbox(rec)[4]-800000), 
+  #                                   label = paste0(n$np, " participations\n",
+  #                                                  n$nv, " villes")),
+  #           size = 2) +
+  # annotate("text", label = "Nom des villes : à partir de 7 participations", 
+  #          size = 2.7, x = 1000000, y = 2800000, hjust = 0) +
+  # annotate("text", label = str_c(sum(sfUrbactCitiesAggrP$NbPart), " participations\n", 
+  #                                length(unique(sfUrbactCitiesAggrP$geonameId)), " villes"),
+  #          size = 3,
+  #          x = c(st_bbox(rec)[3]-1000000), y = c(st_bbox(rec)[4]-800000)) +
+  
+  # geom_sf_text(data = sfUrbactCitiesAggrP %>% filter(NbPart > 6),
+  #              aes(label = asciiName), size = 2.2, color = "black",
+  #              check_overlap = TRUE) +
+  geom_line(data = myScaleBar, aes(x = X, y = Y), size = 0.5, color = "#333333") +
+  
+  annotate("text", label = "500 km", size = 1.5, color = "#333333", hjust = 0,
+           x = c(st_bbox(rec)[3]-800000), y = c(st_bbox(rec)[2]+280000)) +
+  
+  geom_sf(data = rec, fill = NA, color = "ivory4", size = 0.5) +
+  
+  coord_sf(crs = 3035, datum = NA,
+           xlim = st_bbox(rec)[c(1,3)],
+           ylim = st_bbox(rec)[c(2,4)]) +
+  theme_void() +
+  theme(legend.position =  c(0.65, 0.36),
+        legend.title = element_text(size = 8),
+        legend.text = element_text(size = 6),
+        strip.text.x = element_text(size = 12),
+        plot.caption = element_text(size = 5.5, vjust = 15, hjust = 0.97))
+
+
+
+
+
+
+# ===== 8. Fig. 3.?: participation et classe de taille ======
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
